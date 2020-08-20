507e3b84deb5a47ba48279a29f8a34ca2ec734f9
Sean Kelley <seansfkelley@gmail.com>
2020-08-20 01:15:52 -0700

    Lint.

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Graph, Node, Edge } from "../";
 
-export function Demo(props: {}) {
+export function Demo() {
   const [nodes, setNodes] = React.useState<Node[]>([
     { id: "1", x: 0, y: 0 },
     { id: "2", x: 100, y: 100 },
   ]);
-  const [edges, setEdges] = React.useState<Edge[]>([{ sourceId: "1", targetId: "2" }]);
+  const [edges] = React.useState<Edge[]>([{ sourceId: "1", targetId: "2" }]);
 
   const [gridEnabled, setGridEnabled] = React.useState(true);
 
